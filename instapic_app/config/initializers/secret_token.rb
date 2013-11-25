4fcaba565a1e28ea373df41aaf6035b398dbda27
HemangPatel1 <hemang2@gmail.com>
2013-11-25 11:13:16 +0000

    initial commit: created rails app and databases

---
+++ instapic_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstapicApp::Application.config.secret_key_base = 'e8df24a18295d33876df8108d7d6843e3eb7360e50cd3840bd368c03f50ae33e502145b2470da1c26525a7a70c58e4e4fb3ba72978916ffa296177db9db45d7e'
---
+++ instapic_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InstapicApp::Application.config.session_store :cookie_store, key: '_instapic_app_session'
