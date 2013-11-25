0b9d0187e588264d45481d2855f421b9a695bdb1
HemangPatel1 <hemang2@gmail.com>
2013-11-25 20:12:24 +0000

    added userid to pics and user can only delete his own pic

--- instapic_app/app/controllers/pics_controller.rb
+++ instapic_app/app/controllers/pics_controller.rb
@@ -1,6 +1,7 @@
 class PicsController < ApplicationController
 
 before_filter :authenticate_user!
+before_action :check_permission, only: [:destroy]
 
 	def index
 		@pics = Pic.all
@@ -11,7 +12,7 @@ before_filter :authenticate_user!
 	end
 
 	def create
-		@pic = Pic.new(pic_params)
+		@pic = Pic.new((pic_params).merge(user: current_user))
 		@pic.save
 		redirect_to @pic
 	end
@@ -27,6 +28,15 @@ before_filter :authenticate_user!
 	end
 
 private
+	def check_permission
+	   @pic = Pic.find(params[:id])
+
+	  if @pic.user != current_user
+	    flash.alert = "Not your post"
+	    redirect_to pic_path(@pic)
+	  end
+	end
+
 	def pic_params
 		params.require(:pic).permit(:caption, :image)	
 	end
