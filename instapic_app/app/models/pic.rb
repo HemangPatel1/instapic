9a2cfaa2c422c1991c5a18d6441b876a25bf6981
HemangPatel1 <hemang2@gmail.com>
2013-11-25 19:56:37 +0000

    added timeago to posts

--- instapic_app/app/models/pic.rb
+++ instapic_app/app/models/pic.rb
@@ -3,6 +3,7 @@ class Pic < ActiveRecord::Base
 	# validates :caption, presence: true, 
 	# 		  length: { minimum: 5 }
 	has_attached_file :image, 
-  					:styles => { :medium => "	300x300>", :thumb => "100x100>" }, 
+  					:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
   					:default_url => "/images/:style/missing.png"
+  	validates :image, :attachment_presence => true
 end
