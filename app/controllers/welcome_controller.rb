8df9d7602dc1c87ccf69c2710f592bb70793180d
HemangPatel1 <hemang2@gmail.com>
2013-11-28 12:30:04 +0000

    added root redirect if logged in or stay on root to signup or login if not already logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  
   def index
+  	if current_user
+    	redirect_to(pics_path, :notice => "Welcome back!")
+	else
+    	redirect_to root_path
+	end
   end
+
 end
