7f2b87156d25ff986ed0b6d221945599c8fd28bd
HemangPatel1 <hemang2@gmail.com>
2013-12-02 17:27:10 +0000

    added users avatar to new user and edit profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  before_filter :configure_permitted_parameters, :only => [:create, :edit, :update]
+
+
   protected
 
     def after_update_path_for(resource)
       edit_user_registration_path(resource)
     end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :avatar) }
+	  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :current_password, :password, :password_confirmation, :avatar) }
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 InstapicApp::Application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => :registrations }
-
+  resources :users
   resources :charges
   resources :tags
   resources :pics do
